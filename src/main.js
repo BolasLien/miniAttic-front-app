076652ef6806b0f4e42b3dd356a899037efd0e8a
Bolas <ltzuen@gmail.com>
2020-07-06 21:59:46 +0800

    引用套件

--- src/main.js
+++ src/main.js
@@ -5,7 +5,15 @@ import store from './store'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+
 Vue.config.productionTip = false
+
+library.add(faInstagram)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
