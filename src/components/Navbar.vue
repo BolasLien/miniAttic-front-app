893b46f8bcb446f743312cd85c7fafca41a5eefb
BolasLien <ltzuen@gmail.com>
2020-07-06 14:47:37 +0800

    Nav

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,56 +4,59 @@
     <b-container fluid class="bg-dark">
       <b-row class="justify-content-center align-items-center">
         <b-col class="links" cols="2">
-          <a href="#" class="ig">
-            ig
-          </a>
-          <a href="#" class="contact">
-            聯絡我們
-          </a>
+          <a href="#">ig </a>
+          <a href="#">| 聯絡我們 </a>
         </b-col>
         <b-col class="logo" cols="8">
-          <a href="#"><img width="100" src="../assets/logo.png" alt=""></a>
+          <a href="#">
+            <img width="100" src="../assets/logo.png" alt />
+          </a>
         </b-col>
         <b-col class="login" cols="2">
-          <a href="#"></a>
+          <a href="#">註冊 </a>
+          <a href="#">| 登入</a>
+          <a href="#">| 購物車</a>
         </b-col>
       </b-row>
     </b-container>
     <!-- Navigation -->
     <div>
       <b-navbar toggleable="lg" type="dark" variant="dark">
-        <b-navbar-brand href="#">NavBar</b-navbar-brand>
+        <!-- <b-navbar-brand href="#">NavBar</b-navbar-brand> -->
 
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
         <b-collapse id="nav-collapse" is-nav>
-          <b-navbar-nav>
-            <b-nav-item href="#">Link</b-nav-item>
-            <b-nav-item href="#" disabled>Disabled</b-nav-item>
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item href="#">關於我們</b-nav-item>
+            <b-nav-item href="#">最新消息</b-nav-item>
+            <b-nav-item href="#">好評推薦</b-nav-item>
+            <b-nav-item href="#">購買流程</b-nav-item>
+            <!-- <b-nav-item href="#" disabled>Disabled</b-nav-item> -->
           </b-navbar-nav>
 
           <!-- Right aligned nav items -->
           <b-navbar-nav class="ml-auto">
-            <b-nav-form>
+            <!-- <b-nav-form>
               <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
               <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-            </b-nav-form>
+            </b-nav-form> -->
 
-            <b-nav-item-dropdown text="Lang" right>
+            <!-- <b-nav-item-dropdown text="Lang" right>
               <b-dropdown-item href="#">EN</b-dropdown-item>
               <b-dropdown-item href="#">ES</b-dropdown-item>
               <b-dropdown-item href="#">RU</b-dropdown-item>
               <b-dropdown-item href="#">FA</b-dropdown-item>
-            </b-nav-item-dropdown>
+            </b-nav-item-dropdown> -->
 
-            <b-nav-item-dropdown right>
+            <!-- <b-nav-item-dropdown right> -->
               <!-- Using 'button-content' slot -->
-              <template v-slot:button-content>
+              <!-- <template v-slot:button-content>
                 <em>User</em>
               </template>
               <b-dropdown-item href="#">Profile</b-dropdown-item>
               <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-            </b-nav-item-dropdown>
+            </b-nav-item-dropdown> -->
           </b-navbar-nav>
         </b-collapse>
       </b-navbar>
@@ -82,4 +85,5 @@ export default {
   border-bottom: 1px solid #eee;
   color: darkred;
 }
+
 </style>
