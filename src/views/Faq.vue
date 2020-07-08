12b118398d2b14c8f05fd41093358cc737b648f5
BolasLien <ltzuen@gmail.com>
2020-07-08 11:38:20 +0800

    購買流程頁面

--- src/views/Faq.vue
+++ src/views/Faq.vue
@@ -1,25 +1,48 @@
 <template>
   <div id="faq">
-    <Heading title="購買流程" subtitle="How to buy?"></Heading>
-    <div class="container">
-      <h4>訂購需要做的事情</h4>
+    <!-- 麵包屑 -->
+    <b-container>
+      <b-breadcrumb>
+        <b-breadcrumb-item href="/">
+          <b-icon icon="house-fill" scale="1.25" shift-v="1.25"></b-icon>
+        </b-breadcrumb-item>
+        <b-breadcrumb-item>
+          <router-link to="Faq">購買流程</router-link>
+        </b-breadcrumb-item>
+      </b-breadcrumb>
+    </b-container>
 
-    </div>
+    <b-container>
+      <!-- 訂購流程 -->
+      <div class="mt-4">
+        <h4>購買流程</h4>
+        <img src="https://picsum.photos/1080/300/?random=1" />
+      </div>
 
-    <div class="container">
-      <h4>訂購注意事項</h4>
-    </div>
+      <!-- 付款方式 -->
+      <div class="mt-4">
+        <h4>付款方式</h4>
+        <ul>
+          <li>面交付現</li>
+          <li>銀行轉帳/ATM(匯款完成後請告知我們您的帳號末5碼)</li>
+        </ul>
+      </div>
 
-    <div class="container">
-      <h4>付款方式</h4>
-        <h5>面交付現</h5>
-        <h5>銀行轉帳/ATM(匯款完成後回報帳號末5碼，確認後安排出貨)</h5>
-    </div>
+      <!-- 注意事項 -->
+      <div class="mt-4">
+        <h4>注意事項</h4>
+        <ul>
+          <li>下單後會有專人跟您確認面交時間</li>
+          <li>確認訂單後開始製作</li>
+          <li>請留意商品保存方式。常溫：3天。冷藏：7天</li>
+        </ul>
+      </div>
+    </b-container>
   </div>
 </template>
 
 <script>
-import Heading from '@/components/Heading.vue'
+// import Heading from '@/components/Heading.vue'
 export default {
   name: 'About',
   data () {
@@ -28,7 +51,7 @@ export default {
     }
   },
   components: {
-    Heading
+    // Heading
   }
 }
 </script>
