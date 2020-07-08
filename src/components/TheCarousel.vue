5fe6f9ba0bc7adcfd0de988d3065f3962d6718a1
BolasLien <ltzuen@gmail.com>
2020-07-08 15:54:10 +0800

    只留需要的項目

--- src/components/TheCarousel.vue
+++ src/components/TheCarousel.vue
@@ -13,42 +13,8 @@
       @sliding-start="onSlideStart"
       @sliding-end="onSlideEnd"
     >
-      <!-- Text slides with image -->
-      <b-carousel-slide
-        caption="First slide"
-        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
-        img-src="https://picsum.photos/1024/400/?image=52"
-      ></b-carousel-slide>
-
-      <!-- Slides with custom text -->
-      <b-carousel-slide img-src="https://picsum.photos/1024/400/?image=54">
-        <h1>Hello world!</h1>
-      </b-carousel-slide>
-
       <!-- Slides with image only -->
       <b-carousel-slide img-src="https://picsum.photos/1024/400/?image=58"></b-carousel-slide>
-
-      <!-- Slides with img slot -->
-      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
-      <b-carousel-slide>
-        <template v-slot:img>
-          <img
-            class="d-block img-fluid w-100"
-            width="1024"
-            height="400"
-            src="https://picsum.photos/1024/400/?image=55"
-            alt="image slot"
-          >
-        </template>
-      </b-carousel-slide>
-
-      <!-- Slide with blank fluid image to maintain slide aspect ratio -->
-      <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros felis, tincidunt
-          a tincidunt eget, convallis vel est. Ut pellentesque ut lacus vel interdum.
-        </p>
-      </b-carousel-slide>
     </b-carousel>
 
     <!-- <p class="mt-4">
@@ -64,7 +30,8 @@ export default {
   data () {
     return {
       slide: 0,
-      sliding: null
+      sliding: null,
+      carousels: []
     }
   },
   methods: {
