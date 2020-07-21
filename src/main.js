57a5522e09d98bb3db5e70faf6927daacee789c8
Bolas <ltzuen@gmail.com>
2020-07-21 19:08:24 +0800

    註解

--- src/main.js
+++ src/main.js
@@ -2,35 +2,43 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+// bootstrap vue
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+// style.scss
 import './style/style.scss'
 
+// vue-fontawesome
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faInstagram, faFacebookSquare } from '@fortawesome/free-brands-svg-icons'
 import { faShoppingCart, faUser, faSignInAlt, faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
 
+// vue axios
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
+// vue gtag
 import VueGtag from 'vue-gtag'
 
-axios.defaults.withCredentials = true
-Vue.use(VueAxios, axios)
-
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 library.add(faInstagram, faShoppingCart, faUser, faSignInAlt, faPlus, faMinus, faFacebookSquare)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
+axios.defaults.withCredentials = true
+Vue.use(VueAxios, axios)
+
 Vue.use(VueGtag, {
   config: { id: 'G-NXGJ8V0XNM' }
 })
+
 new Vue({
   router,
   store,
