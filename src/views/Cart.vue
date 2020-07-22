3fc2b7dc77df1cf75440c34df792ca3f636cf075
BolasLien <ltzuen@gmail.com>
2020-07-22 16:25:27 +0800

    購物車頁面

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,92 +1,265 @@
 <template>
   <div id="cart">
-    <b-container class="shopping-step mt-4 mb-4">
-      <b-row>
-        <b-col sm="4">
-          第1步
-          <br />購物車確認
-        </b-col>
-        <b-col sm="4">
-          第2步
-          <br />付款與運送方式
-        </b-col>
-        <b-col sm="4">
-          第3步
-          <br />完成訂購
-        </b-col>
-      </b-row>
-    </b-container>
+    <b-container class="shopping-step mt-4 mb-5">
+      <form-wizard title subtitle @on-complete="submit" color="#555b6e" class="p-n5">
+        <tab-content :title="steps[0]">
+          <b-container class="cart">
+            <b-row style="height:50px;" class="cart-head">
+              <b-col sm="12">以下是您選購的商品</b-col>
+            </b-row>
+            <b-row
+              style="height:150px;"
+              class="cart-item"
+              v-for="data in shoppigList"
+              :key="data.item"
+            >
+              <b-col sm="3">
+                <b-img width="120" :src="data.src"></b-img>
+              </b-col>
+              <b-col sm="3">{{data.name}}</b-col>
+              <b-col sm="2">
+                <quantity v-model="data.amount"></quantity>
+              </b-col>
+              <b-col sm="3">
+                <small>NT$&ensp;</small>
+                {{data.price * data.amount}}
+              </b-col>
+              <b-col sm="1">
+                <button class="btn btn-remove" @click="remove(data)">X</button>
+              </b-col>
+            </b-row>
+          </b-container>
+        </tab-content>
+        <tab-content :title="steps[1]">
+          <b-container class="cart">
+            <b-row style="height:50px;" class="cart-head">
+              <b-col sm="12">請選擇您的付款方式</b-col>
+            </b-row>
 
-    <b-container class="shopping-cart-container">
-      <b-table-simple>
-        <b-tr v-for="data in datas" :key="data.item">
-          <b-td style=" width:25%"  class="align-middle">
-            <b-img thumbnail width="120" :src="data.src"></b-img>
-          </b-td>
-          <b-td style=" width:25%" class="align-middle">{{data.name}}</b-td>
-          <b-td style=" width:25%" class="align-middle">{{data.count}}</b-td>
-          <b-td style=" width:25%" class="align-middle">
-            <b-button>X</b-button>
-            <br>
-            {{data.price * data.count}}
-            </b-td>
-        </b-tr>
+            <b-row
+              style="height:100px;"
+              class="cart-item"
+              v-for="data in payments"
+              :key="data.item"
+            >
+              <b-col sm="3" class="text-right">
+                <input
+                  type="radio"
+                  name="payment"
+                  @input="paymentSelect(data)"
+                  :checked="data.selected"
+                />
+              </b-col>
+              <b-col sm="3">{{data.name}}</b-col>
+              <b-col sm="2">{{data.description}}</b-col>
+              <b-col sm="3">
+                <small>+NT$&ensp;</small>
+                {{data.price}}
+              </b-col>
+              <b-col sm="1"></b-col>
+            </b-row>
+          </b-container>
+        </tab-content>
+        <tab-content :title="steps[2]">
+          <b-container class="order">
+            <b-row style="height:50px;" class="order-head">
+              <b-col sm="12">請確認您的訂單</b-col>
+            </b-row>
 
-                  <!-- <b-tr>
-            <b-td>
-              <b-row class="align-items-center">
-                <b-col sm="3">
-                  <b-img thumbnail rounded="0" src="https://picsum.photos/800/600/?image=20"></b-img>
-                </b-col>
-                <b-col sm="5">商品名稱</b-col>
-                <b-col sm="2">商品數量</b-col>
-                <b-col sm="2">
-                  <b-button>X</b-button>
-                  <br />商品價格
-                </b-col>
-              </b-row>
-            </b-td>
-          </b-tr> -->
-        <b-tfoot>
-          <tr>
-            <b-td colspan="2"></b-td>
-            <b-td colspan="2">總計:1231321</b-td>
-            </tr>
+            <b-row
+              style="height:120px;"
+              class="order-item"
+              v-for="data in shoppigList"
+              :key="data.item"
+            >
+              <b-col sm="3">
+                <b-img width="80" :src="data.src"></b-img>
+              </b-col>
+              <b-col sm="3">{{data.name}}</b-col>
+              <b-col sm="2">{{data.amount}}</b-col>
+              <b-col sm="3">
+                <small>NT$&ensp;</small>
+                {{data.price * data.amount}}
+              </b-col>
+              <b-col sm="1"></b-col>
+            </b-row>
 
-        </b-tfoot>
-      </b-table-simple>
+            <b-row style="height:50px;" class="order-item">
+              <b-col sm="3">付款及運送方式</b-col>
+              <b-col sm="3">{{payment.name}}</b-col>
+              <b-col sm="2">{{payment.description}}</b-col>
+              <b-col sm="3">
+                <small>NT$&ensp;</small>
+                {{payment.price}}
+              </b-col>
+              <b-col sm="1"></b-col>
+            </b-row>
+          </b-container>
+        </tab-content>
+        <b-container>
+          <b-row class="justify-content-end mt-4 cart-footer">
+            <b-col sm="2">共&ensp;{{ totalAmount}}&ensp;件商品</b-col>
+            <b-col sm="2" class="text-right">商品金額</b-col>
+            <b-col sm="2" class="text-left">
+              <small>NT$&ensp;</small>
+              {{ totalPrice }}
+            </b-col>
+          </b-row>
+          <b-row class="justify-content-end mt-4 cart-footer">
+            <b-col sm="2" class="text-right">運費</b-col>
+            <b-col sm="2" class="text-left">
+              <small>NT$&ensp;</small>
+              {{ payment.price }}
+            </b-col>
+          </b-row>
+          <b-row class="justify-content-end mt-4 mb-4 cart-footer">
+            <b-col sm="2" class="text-right payable">應付金額</b-col>
+            <b-col sm="2" class="text-left payable">NT$&ensp; {{ totalPrice+payment.price }}</b-col>
+          </b-row>
+        </b-container>
+
+        <template slot="footer" slot-scope="props">
+          <div class="wizard-footer-left">
+            <wizard-button
+              v-if="props.activeTabIndex > 0"
+              @click.native="props.prevTab()"
+              :style="props.fillButtonStyle"
+            >返回</wizard-button>
+          </div>
+          <div class="wizard-footer-right">
+            <wizard-button
+              v-if="!props.isLastStep"
+              @click.native="props.nextTab()"
+              class="wizard-footer-right"
+              :style="props.fillButtonStyle"
+            >下一步</wizard-button>
+
+            <wizard-button
+              v-else
+              @click.native="submit"
+              class="wizard-footer-right finish-button"
+              :style="props.fillButtonStyle"
+            >{{props.isLastStep ? '送出' : '下一步'}}</wizard-button>
+          </div>
+        </template>
+      </form-wizard>
     </b-container>
   </div>
 </template>
 
 <script>
+import Quantity from '../components/Quantity'
 export default {
+  components: {
+    quantity: Quantity
+  },
   data () {
     return {
-      datas: [
+      steps: ['確認商品', '選擇付款與運送方式', '送出訂單'],
+      currentStep: 0,
+      shoppigList: [
         {
           item: 1,
-          name: '好吃1',
-          src: 'https://picsum.photos/800/800/?image=20',
-          count: 3,
-          price: 480
-        },
-        {
-          item: 3,
-          name: '好吃3',
-          src: 'https://picsum.photos/800/800/?image=21',
-          count: 2,
+          name: '布朗尼 | 草莓',
+          src:
+            'http://220.128.133.15/s1090109/miniattic/assets/img/1594965691254.jpg',
+          amount: 2,
           price: 480
         },
         {
           item: 2,
-          name: '好吃2',
-          src: 'https://picsum.photos/800/800/?image=22',
-          count: 3,
+          name: '蛋糕 | 檸檬',
+          src:
+            'http://220.128.133.15/s1090109/miniattic/assets/img/1595310976856.jpg',
+          amount: 1,
           price: 480
+        },
+        {
+          item: 3,
+          name: '餅乾 | 芥末',
+          src:
+            'http://220.128.133.15/s1090109/miniattic/assets/img/1595310987348.jpg',
+          amount: 3,
+          price: 580
         }
-      ]
+      ],
+      payment: {
+        item: 1,
+        name: '面交付款',
+        price: 0
+      },
+      payments: [
+        {
+          item: 1,
+          name: '面交付款',
+          price: 0,
+          selected: true
+        },
+        {
+          item: 2,
+          name: '7-11取貨付款',
+          price: 60,
+          description: '部分商品不適用',
+          selected: false
+        },
+        {
+          item: 3,
+          name: '宅配貨到付款',
+          price: 120,
+          description: '皆使用低溫宅急便',
+          selected: false
+        }
+      ],
+      paymentPrice: 0
+
+    }
+  },
+  methods: {
+    remove (data) {
+      this.shoppigList.splice(this.shoppigList.indexOf(data), 1)
+    },
+    submit () {
+      console.log(this.order)
+      alert('uuuuuuuuuuuuuuuuuu')
+    },
+    paymentSelect (data) {
+      this.payment = data
+      console.log(this.payment)
+    }
+  },
+  computed: {
+    totalAmount () {
+      return this.shoppigList
+        .map(e => e.amount)
+        .reduce(function (prev, e) {
+          return prev + e
+        })
+    },
+    totalPrice () {
+      let count = 0
+      for (const data of this.shoppigList) {
+        count += data.price * data.amount
+      }
+      return count + this.paymentPrice
+    },
+    order () {
+      const cart = []
+      for (const e of this.shoppigList) {
+        cart.push({
+          item: e.item,
+          name: e.name,
+          amout: e.amount
+        })
+      }
+
+      return [cart, this.payment]
     }
   }
 }
 </script>
+
+<style scoped>
+td {
+  border: 1px solid #000 !important;
+}
+</style>
