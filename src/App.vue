65f9a574d7c20f5b67dcf3e8efd2e572afe8df86
BolasLien <ltzuen@gmail.com>
2020-07-23 16:31:19 +0800

    heartbeat

--- src/App.vue
+++ src/App.vue
@@ -26,15 +26,52 @@ export default {
       webdata: []
     }
   },
+  computed: {
+    user () {
+      return this.$store.getters.user
+    }
+  },
   methods: {
     reload () {
       this.isRouterAlive = false
       this.$nextTick(() => {
         this.isRouterAlive = true
       })
+    },
+    heartbeat () {
+      this.axios
+        .get(process.env.VUE_APP_API + '/heartbeat')
+        .then(response => {
+          const data = response.data
+
+          if (this.user.length > 0) {
+            if (!data) {
+              alert('登入時效已過')
+              // 前端登出
+              this.$store.commit('logout')
+              // 如果現在不是在首頁，跳到登出後的首頁
+              if (this.$route.path !== '/') {
+                this.$router.push('/')
+              }
+            }
+          }
+        })
+        .catch(error => {
+          console.log(error)
+          // alert('發生錯誤')
+          this.$store.commit('logout')
+          // 如果現在不是在首頁，跳到登出後的首頁
+          if (this.$route.path !== '/') {
+            this.$router.push('/')
+          }
+        })
     }
   },
   mounted () {
+    this.heartbeat()
+    setInterval(() => {
+      this.heartbeat()
+    }, 1000 * 5)
     this.axios.get(process.env.VUE_APP_API + '/webdata')
       .then(response => {
         this.webdata = response.data
