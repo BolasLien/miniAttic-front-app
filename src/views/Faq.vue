8649568c5caff2bcafc02e526f8309b1d907a272
BolasLien <ltzuen@gmail.com>
2020-07-28 16:31:12 +0800

    購物流程

--- src/views/Faq.vue
+++ src/views/Faq.vue
@@ -15,28 +15,26 @@
     <b-container>
       <!-- 訂購流程 -->
       <div class="mt-4">
-        <h4>購買流程</h4>
-        <img src="https://picsum.photos/1080/300/?random=1" />
-      </div>
-
-      <!-- 付款方式 -->
-      <div class="mt-4">
-        <h4>付款方式</h4>
-        <ul>
-          <li>面交付現</li>
-          <li>銀行轉帳/ATM(匯款完成後請告知我們您的帳號末5碼)</li>
-        </ul>
+        <b-row class="step-item" v-for="(item, index) in steps" :key="index">
+          <b-col lg="3" sm="12">
+            <font-awesome-icon :icon="item.icon" size="5x"></font-awesome-icon>
+          </b-col>
+          <b-col lg="3" sm="12">
+            <h6>step{{item.step}}</h6>
+            <h5>{{item.text}}</h5>
+          </b-col>
+        </b-row>
       </div>
 
       <!-- 注意事項 -->
-      <div class="mt-4">
+      <!-- <div class="mt-4 text-left">
         <h4>注意事項</h4>
         <ul>
           <li>下單後會有專人跟您確認面交時間</li>
           <li>確認訂單後開始製作</li>
           <li>請留意商品保存方式。常溫：3天。冷藏：7天</li>
         </ul>
-      </div>
+      </div> -->
     </b-container>
   </div>
 </template>
@@ -49,7 +47,33 @@ export default {
   name: 'About',
   data () {
     return {
-      user: ''
+      steps: [
+        {
+          step: 1,
+          icon: ['fas', 'thumbs-up'],
+          text: '挑選喜歡的商品'
+        },
+        {
+          step: 2,
+          icon: ['fas', 'shopping-cart'],
+          text: '將商品加入購物車'
+        },
+        {
+          step: 3,
+          icon: ['fas', 'truck'],
+          text: '選擇付款與運送方式'
+        },
+        {
+          step: 4,
+          icon: ['fas', 'receipt'],
+          text: '送出訂單'
+        },
+        {
+          step: 5,
+          icon: ['fas', 'check'],
+          text: '訂購完成'
+        }
+      ]
     }
   }
 }
