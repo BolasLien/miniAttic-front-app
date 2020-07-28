2c9232308f620b7600ca4f5688bc3e9f375fb4b0
BolasLien <ltzuen@gmail.com>
2020-07-28 16:31:05 +0800

    排版

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="about">
-        <!-- 麵包屑 -->
+    <!-- 麵包屑 -->
     <b-container>
       <b-breadcrumb>
         <b-breadcrumb-item href="/">
@@ -12,22 +12,17 @@
       </b-breadcrumb>
     </b-container>
 
+    <!-- <b-container v-html="about"> -->
     <b-container>
-      <img src="https://picsum.photos/1280/450/?random=1" />
-      <h4 class="mt-3">小閣樓的開始</h4>
-      <p>miniAttic是在「小閣樓」裡做甜點時誕生的品牌，透過手做的甜點傳達溫暖的祝福，希望每個品嘗過miniAttic甜點的人，能身處在一個悠閒的午後，窩在自己的秘密基地裡，一個人享受甜點的美好時光。</p>
-    </b-container>
-
-    <b-container>
-      <img src="https://picsum.photos/1280/450/?random=2" />
-      <h4 class="mt-3">小閣樓的堅持</h4>
-      <p>小閣樓重複嘗試過各式各樣的材料，以最單純的原料，最平衡的味道，才能誕生的每一份祝福。</p>
-    </b-container>
-
-    <b-container>
-      <img src="https://picsum.photos/1280/450/?random=3" />
-      <h4 class="mt-3">小閣樓的成長</h4>
-      <p>每個支持過「小閣樓」的人們，感謝你們對小閣樓的信任，小閣樓才能越來越茁壯。</p>
+      <b-row class="about-item" v-for="(post,index) in posts" :key="index">
+        <b-col lg="5" sm="12" :order-lg="index %2 === 0 ? 2:1" order-sm="1">
+          <img :src="post.img" />
+        </b-col>
+        <b-col lg="5" sm="12" :order-lg="index %2 === 0 ? 1:2" order-sm="2">
+          <h4 class="mt-3">{{post.title}}</h4>
+          <p>{{post.article}}</p>
+        </b-col>
+      </b-row>
     </b-container>
   </div>
 </template>
@@ -37,22 +32,31 @@ export default {
   props: {
     webdata: Object
   },
-  name: 'About',
   data () {
     return {
-
+      posts: [
+        {
+          img: 'http://220.128.133.15/s1090109/miniattic/assets/img/about/1.jpg',
+          title: '小閣樓的開始',
+          article: 'miniAttic是在「小閣樓」裡做甜點時誕生的品牌，透過手做的甜點傳達溫暖的祝福，希望每個品嘗過miniAttic甜點的人，能身處在一個悠閒的午後，窩在自己的秘密基地裡，一個人享受甜點的美好時光。'
+        },
+        {
+          img: 'http://220.128.133.15/s1090109/miniattic/assets/img/about/2.jpg',
+          title: '小閣樓的堅持',
+          article: '小閣樓重複嘗試過各式各樣的材料，以最單純的原料，最平衡的味道，才能誕生的每一份祝福。'
+        },
+        {
+          img: 'http://220.128.133.15/s1090109/miniattic/assets/img/about/3.jpg',
+          title: '小閣樓的成長',
+          article: '感謝每個支持過「小閣樓」的人們，因為有你們對小閣樓的信任，小閣樓才能越來越茁壯。'
+        }
+      ]
     }
   },
-  components: {
-
+  methods: {
+    // order(index){
+    //   return
+    // }
   }
 }
 </script>
-
-<style scoped>
-img {
-  display: block;
-  max-width: 100%;
-}
-
-</style>
