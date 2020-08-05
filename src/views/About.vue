7eddb951919a57cc9fb19fd913362e288838edb4
BolasLien <ltzuen@gmail.com>
2020-08-05 14:25:59 +0800

    改圖片路徑

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,8 +3,8 @@
     <!-- 麵包屑 -->
     <b-container>
       <b-breadcrumb>
-        <b-breadcrumb-item to="/">
-          <b-icon icon="house-fill" scale="1.25" shift-v="1.25"></b-icon>
+        <b-breadcrumb-item>
+          <router-link to="/"><b-icon icon="house-fill" scale="1.25" shift-v="1.25"></b-icon></router-link>
         </b-breadcrumb-item>
         <b-breadcrumb-item>
           <router-link to="About">關於我們</router-link>
@@ -36,17 +36,17 @@ export default {
     return {
       posts: [
         {
-          img: 'http://220.128.133.15/s1090109/miniattic/assets/img/about/1.jpg',
+          img: process.env.VUE_APP_API + '/image/' + 'about-1.jpg',
           title: '小閣樓的開始',
           article: 'miniAttic是在「小閣樓」裡做甜點時誕生的品牌，透過手做的甜點傳達溫暖的祝福，希望每個品嘗過miniAttic甜點的人，能身處在一個悠閒的午後，窩在自己的秘密基地裡，一個人享受甜點的美好時光。'
         },
         {
-          img: 'http://220.128.133.15/s1090109/miniattic/assets/img/about/2.jpg',
+          img: process.env.VUE_APP_API + '/image/' + 'about-2.jpg',
           title: '小閣樓的堅持',
           article: '小閣樓重複嘗試過各式各樣的材料，以最單純的原料，最平衡的味道，才能誕生的每一份祝福。'
         },
         {
-          img: 'http://220.128.133.15/s1090109/miniattic/assets/img/about/3.jpg',
+          img: process.env.VUE_APP_API + '/image/' + 'about-3.jpg',
           title: '小閣樓的成長',
           article: '感謝每個支持過「小閣樓」的人們，因為有你們對小閣樓的信任，小閣樓才能越來越茁壯。'
         }
--- src/views/News.vue
+++ src/views/News.vue
@@ -3,8 +3,8 @@
         <!-- 麵包屑 -->
     <b-container>
       <b-breadcrumb>
-        <b-breadcrumb-item href="/">
-          <b-icon icon="house-fill" scale="1.25" shift-v="1.25"></b-icon>
+        <b-breadcrumb-item >
+          <router-link to="/"><b-icon icon="house-fill" scale="1.25" shift-v="1.25"></b-icon></router-link>
         </b-breadcrumb-item>
         <b-breadcrumb-item>
           <router-link to="News">最新消息</router-link>
@@ -41,23 +41,23 @@ export default {
       posts: [
         {
           item: 1,
-          img: 'http://220.128.133.15/s1090109/miniattic/assets/img/post/1.jpg',
-          title: '布朗尼「們」出發了👏👏👏',
-          article: '明天是一個特別的日子，我們趕在今天內把新鮮現做的布朗尼送到樓主們的手中，讓各位嚐嚐我們的厲害😏。<br>祝全天下的媽媽母親節快樂👩🎁🎉',
+          img: process.env.VUE_APP_API + '/image/' + 'post-1.jpg',
+          title: '布朗尼「們」出發了',
+          article: '明天是一個特別的日子，我們趕在今天內把新鮮現做的布朗尼送到樓主們的手中，讓各位嚐嚐我們的厲害。<br>祝全天下的媽媽母親節快樂',
           date: '2020年5月9日',
           tag: '小閣樓點滴'
         },
         {
           item: 2,
-          img: 'http://220.128.133.15/s1090109/miniattic/assets/img/post/2.jpg',
-          title: '小閣樓三個月快樂🥳🎉',
-          article: '默默的也成立了三個月了，這個過程中，很感謝有很多親友的支持，我們才可以走到今天。<br>當然如果只是現階段就滿足了，小閣樓的成長可能就停滯了。所以，我們每個禮拜都在出爐新甜點、試吃新甜點。<br>體重會證明我們真的很努力😭',
+          img: process.env.VUE_APP_API + '/image/' + 'post-2.jpg',
+          title: '小閣樓三個月快樂',
+          article: '默默的也成立了三個月了，這個過程中，很感謝有很多親友的支持，我們才可以走到今天。<br>當然如果只是現階段就滿足了，小閣樓的成長可能就停滯了。所以，我們每個禮拜都在出爐新甜點、試吃新甜點。<br>體重會證明我們真的很努力',
           date: '2020年5月3日',
           tag: '小閣樓點滴'
         },
         {
           item: 3,
-          img: 'http://220.128.133.15/s1090109/miniattic/assets/img/post/3.jpg',
+          img: process.env.VUE_APP_API + '/image/' + 'post-3.jpg',
           title: '每一次出爐都是一種祝福',
           article: '烤箱打開的那一剎那，總會想著這一次的布朗尼又會去哪裡旅行呢⋯⋯',
           date: '2020年4月19日',
